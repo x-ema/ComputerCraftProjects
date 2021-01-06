833cc3f1e918db50857db705eb1e45c2a6c6b1c6
ZeeDerp <zeederpmaster@gmail.com>
2021-01-06 16:15:07 -0600

    hopefully added chatcolors

--- Glasses.lua
+++ Glasses.lua
@@ -69,9 +69,9 @@ function startNewNew()
       		l = 10 + (i * 10)
 			  s = messages[i]
 			  if s:lower():sub(1,3) == "zee" then
-			  		glass.addText(5, l, s, chatColors[1].[2])
+			  		glass.addText(5, l, s, chatColors[1][2])
 			  	elseif s:lower():sub(1,3) == "sle" then
-					glass.addText(5, l, s, chatColors[2].[2])
+					glass.addText(5, l, s, chatColors[2][2])
 				else
 					glass.addText(5, l, s, 0xFFF000)
 				end
