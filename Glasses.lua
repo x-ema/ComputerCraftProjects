5ad2765a322ed668e6d2d22e57e7fa44472f43f4
ZeeDerp <zeederpmaster@gmail.com>
2021-01-06 19:27:11 -0600

    Starting command parser

--- Glasses.lua
+++ Glasses.lua
@@ -98,15 +98,17 @@ function parseCommand(cmd, usr)
 			table.remove(messages,i)
             table.insert(messages,"$$$$")
         end
-    elseif cmd:lower():sub(1,9) == "maxlines " then
+	elseif cmd:lower():sub(1,9) == "maxlines " then
+		maxLines = cmd:sub(10,11)
 		for i = 1,maxLines do
 			table.remove(messages,i)
             table.insert(messages,"$$$$")
 		end
 		sleep(.1)
-		maxLines = cmd:sub(10,11)
+
 	elseif cmd:lower():sub(1,10) == "chatcolor " then
-		color = cmd:sub(11,17)
+		color = cmd:sub(11,19)
+		chatColors[usr] = color
 	else 
 	mesg = tostring(cmd)
     user = tostring(usr)
