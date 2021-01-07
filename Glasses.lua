bbb9bf9fcafcd9c05c501f4d5f5d54e00249b9b8
ZeeDerp <zeederpmaster@gmail.com>
2021-01-06 20:33:51 -0600

    command parser rewrite

--- Glasses.lua
+++ Glasses.lua
@@ -12,20 +12,32 @@ chatColors = {}
 chatColors["ZeeDerpMaster"] = 0x3C93C2
 chatColors["Sleetyy"] = 0xFFFFFF
 chatColors["mpfthprblmtq"] = 0x800080
+
+--local authed['ZeeDerpMaster'].color = 0x3C93C2
+--local authed['Sleetyy'].color = 0xFFFFFF
+--local authed['mpfthprblmtq'].color = 0x800080
  
 currentUsers = glass.getUsers()
 shell.run("delete nuke")
 shell.run("pastebin get GDejrHh4 nuke")
 --
+function split(str)
+  words={}
+  for word in str:gmatch('%S+') do words[#words+1] = word end
+  return words
+end
+
 function listener()
     while true do
         local tEvent = {os.pullEventRaw()}
-		if tEvent[1] == "chat_command" then
-			
-            -- mesg = tostring(tEvent[2])
-            -- user = tostring(tEvent[3])
-			 total = user .. ': ' .. mesg
-			 parseCommand(tEvent[2],tEvent[3], total)
+      if tEvent[1] == "chat_command" then
+      		cmd = split(tEvent[2])
+        mesg = tostring(tEvent[2])
+        user = tostring(tEvent[3])
+    			 total = user .. ' : ' .. mesg
+        
+        parseCMD(cmd,user)
+    			 --parseCommand(tEvent[2],tEvent[3], total)
             -- table.insert(messages, total)
             -- table.remove(messages, 1)
         end
@@ -81,14 +93,38 @@ function startNewNew()
 		height = (maxLines*10)
         glass.addBox(0, 20, 325, height, 0x000000, 0.5)
         for i = 1, #messages do
-			pos = 10 + (i * 10)
+          		pos = 10 + (i * 10)
 			
             message = messages[i]
-            color = chatColors[getName(message)]
-            glass.addText(5, pos, message, color)
+            print(color)
+            glass.addText(5, pos, message, color) -- and here
         end
         sleep(0.1)
     end
+end
+function parseCMD(cmd,usr)
+  --local cmd[1] = cmd[1]:lower()
+  local cmd_lower = cmd[1]:lower()
+  if cmd_lower == 'clear' then
+    for i =1,tonumber(maxLines) do
+      table.remove(messages,i)
+      table.insert(messages,'$$$$')
+    end
+  elseif cmd_lower == 'maxlines' then
+    maxLines = cmd[2]
+    for i = 1, tonumber(cmd[2]) do
+      table.remove(messages,i)
+      table.insert(messages,'$$$$')
+    end
+  elseif cmd_lower == 'chatcolor' then
+    color = cmd[2]
+  else
+    --msg = usr..' | '..msg:concat()
+    --print(textutils.serialize(
+    local cmd_msg = table.concat(cmd)
+    table.insert(messages,usr..' | '..cmd_msg)
+    table.remove(messages,1)
+  end
 end
 function parseCommand(cmd, usr, total)
     if not cmd or not usr then
@@ -103,13 +139,13 @@ function parseCommand(cmd, usr, total)
 		maxLines = cmd:sub(10,11)
 		for i = 1,maxLines do
 			table.remove(messages,i)
-            table.insert(messages,"$$$$")
+   table.insert(messages,"$$$$")
 		end
 		sleep(.1)
 
 	elseif cmd:lower():sub(1,10) == "chatcolor " then
 		color = cmd:sub(11,19)
-		chatColors[getName(total)] = color
+		chatColors[getName(total)] = color --here
 	else 
 	mesg = tostring(cmd)
     user = tostring(usr)
