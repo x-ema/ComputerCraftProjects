44f247d637ed4f1d989d6154821004eaa125ac98
ZeeDerp <zeederpmaster@gmail.com>
2021-01-09 01:48:39 -0600

    First Iteration

--- baseNode.lua
+++ baseNode.lua
@@ -2,4 +2,7 @@
 -- Variable per node
 -- if node == true, start writing, when false, clear. ezpz
 sensor = peripheral.wrap("top")
+print("What node is this?")
+input = read()
+getfenv(("".gsub).input = false
 
