07a81cc6336ff7761f0a78669f39de26b56d8cfe
ZeeDerp <zeederpmaster@gmail.com>
2021-01-08 23:26:04 -0600

    Testing printing players

--- ProximityWelcome.lua
+++ ProximityWelcome.lua
@@ -1,3 +1,22 @@
 sensor = peripheral("top")
 authedusers = {"ZeeDerpMaster","Sleetyy"}
 closePlayers = sensor.getPlayerNames()
+
+--
+function table.contains(tab, ele)
+    for _, value in pairs(tab) do
+        if value == ele then
+            return true
+        end
+    end
+    return false
+end
+--
+
+for i=1,#closePlayers do
+    if table.contains(authedusers,closePlayers[i])
+        print('Welcome '..closePlayers[i])
+        print('')
+else
+    print("Frick off")
+end
