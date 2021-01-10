f92e49fa2d54c077b3c40663ffc242eb30018938
ZeeDerp <zeederpmaster@gmail.com>
2021-01-10 01:10:04 -0600

    complete rewrite

--- GlassBaseMap.lua
+++ GlassBaseMap.lua
@@ -6,23 +6,12 @@
 -- Not illegal, doesn't keep a log.
 -- Authorized user protection >:D
 -- Wish me luck :)
-getfenv(("").gsub).playerList = {}
 sensorData = {}
 glass = peripheral.wrap("right")
-masterPlayerTable = getfenv(("").gsub).playerList
-nodenum = 1
-repeat 
-    current = getfenv(('node'..nodenum).gsub).prox
-    if current then
-        for i,v in pairs(masterPlayerTable) do 
-            print(v)
-        end
-    end
-    if nodenum > 32 then 
-        nodenum = 1
-    else
-    nodenum = nodenum + 1
-    end
-    print(nodenum)
-    sleep(.1)
-until x == nil
+rednet.open("top")
+rednet.broadcast("
+    sensor = peripheral.wrap("top")
+    if #sensor.getPlayerNames() > 0
+
+
+")
--- baseNode.lua
+++ baseNode.lua
@@ -1,3 +1,6 @@
+---DEPRACATED---
+
+
 -- This is the node that will be distributed around the base.
 -- Variable per node
 -- if node == true, start writing, when false, clear. ezpz
