b3b7e55b273fd489c0d252b5e410c8f2f0842033
ZeeDerp <zeederpmaster@gmail.com>
2021-01-11 16:06:16 -0600

    ugh

--- GlassBaseMap.lua
+++ GlassBaseMap.lua
@@ -2,27 +2,48 @@
 -- Small Scale display of top down area of base
 -- Sensor nodes every 16 blocks around the edge
 -- When someone enters the influence of a node, they are displayed on glasses
--- Not illegal, doesnt use sensor.sonicScan()
+-- Not illegal, doesnt use coordinate based tracking
 -- Not illegal, doesn't keep a log.
 -- Authorized user protection >:D
 -- Wish me luck :)
-getfenv(("").gsub).playerList = {}
-sensorData = {}
+whitelist = {"ZeeDerpMaster","Sleetyy"}
 glass = peripheral.wrap("right")
-masterPlayerTable = getfenv(("").gsub).playerList
-nodenum = 1
-repeat 
-    x = getfenv(('node'..nodenum).gsub).prox
-    if x == true then
-        for i,v in pairs(masterPlayerTable) do 
-            print(v)
+getfenv(("").gsub).playerList = {}
+nodes = {}
+--
+for i=1,#getfenv(("").gsub).playerList do 
+table.remove(getfenv(("").gsub).playerList,1)
+end
+--
+function table.contains(tab, ele)
+    for _, value in pairs(tab) do
+        if value == ele then
+            return true
         end
     end
-    if nodenum > 32 then 
-        nodenum = 1
-    else
-    nodenum = nodenum + 1
+    return false
+end
+--
+
+
+
+--
+getfenv(("").gsub).proxyVar = "id = os.getComputerID() sensor = peripheral.wrap('top') players = sensor.getPlayerNames() while true do if #players > 0 then for i,v in pairs(players) do table.insert(getfenv(('').gsub).playerList,players[i]..' '..id) print(getfenv(('').gsub).playerList[i]) end end sleep(1) end"
+sleep(1)
+while true do
+    for i,v in pairs(getfenv(("").gsub).playerList) do 
+        d = string.find(getfenv(("").gsub).playerList[i],1)
+        print(string.sub(getfenv(("").gsub).playerList[i],1,(d-1)))
+        if table.contains(whitelist,string.sub(getfenv(("").gsub).playerList[i],1,(d-1))) then
+            getfenv(("").gsub).playerList[i] = nil
+        end
+        realID = string.sub(getfenv(("").gsub).playerList[i],d,(d+4))
+        nodes[i] = realID
+        print(string.sub(getfenv(("").gsub).playerList[i],1,(d-1)).." is at "..realID)
+        if i > 10 then
+            i = 1
+        end
     end
-    print(nodenum)
+    nodenum = nodenum + 1
     sleep(.1)
-until x == nil
+end
--- baseNode.lua
+++ baseNode.lua
@@ -1,26 +1,4 @@
+---DEPRACATED---
 -- This is the node that will be distributed around the base.
--- Variable per node
--- if node == true, start writing, when false, clear. ezpz
-sensor = peripheral.wrap("top")
-closePlayers = sensor.getPlayerNames()
-masterPlayerTable = getfenv(("").gsub).playerList
-print("What node is this?")
-input = read()
-getfenv((input).gsub).prox = false
-while true do
-if #closePlayers > 0 then
-    getfenv((input).gsub).prox = true
-    for i=1,#closePlayers do
-        table.insert(masterPlayerTable,closePlayers[i])
-        if #closePlayers == 0 then
-            getfenv((input).gsub).prox = false
-            getfenv(("").gsub).playerList = {}
-            break
-        end
-    end
-end
-sleep(.1)
-end
-
-
-
+prog = getfenv(("").gsub).proxyVar
+loadstring(prog)()
