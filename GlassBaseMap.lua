37c666527b9d250b9c57310bb9ca0f2b0c64a0cd
ZeeDerp <zeederpmaster@gmail.com>
2021-01-11 01:52:14 -0600

    Brand new big brain strats

--- GlassBaseMap.lua
+++ GlassBaseMap.lua
@@ -11,13 +11,17 @@ glass = peripheral.wrap("right")
 for i=1,#getfenv(("").gsub).playerList do 
 table.remove(getfenv(("").gsub).playerList,1)
 end
-getfenv(("").gsub).proxyVar = "id = os.getComputerID() sensor = peripheral.wrap('top') players = sensor.getPlayerNames() while true do if #players > 0 then for i,v in pairs(players) do if players[i] == "ZeeDerpMaster" or "Sleetyy" then table.remove(players,i) else table.insert(getfenv(('').gsub).playerList,players[i]..' '..id) print(getfenv(('').gsub).playerList[i]) end end end for i,v in pairs(players) do print(v) end sleep(.1) end"
+getfenv(("").gsub).proxyVar = "id = os.getComputerID() sensor = peripheral.wrap('top') players = sensor.getPlayerNames() while true do if #players > 0 then for i,v in pairs(players) do table.insert(getfenv(('').gsub).playerList,players[i]..' '..id) print(getfenv(('').gsub).playerList[i]) end end for i,v in pairs(players) do print(v) end sleep(.1) end"
 sleep(5)
 while true do
     for i,v in pairs(getfenv(("").gsub).playerList) do 
         d = string.find(getfenv(("").gsub).playerList[i],1)
+        if string.sub(getfenv(("").gsub).playerList[i],1,(d-1)) == "ZeeDerpMaster" or "Sleetyy" then
+            i = i + 1
+        else
         realID = string.sub(getfenv(("").gsub).playerList[i],d,(d+4))
         print(getfenv(("").gsub).playerList[i].." is at "..realID)
     end
+end
     sleep(.1)
 end
