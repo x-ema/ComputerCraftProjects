0ddf70872c9a6b1d126e7b0aeda88dda3189abb1
ZeeDerp <zeederpmaster@gmail.com>
2021-01-11 16:18:02 -0600

    heck yes, we gonna get this workin

--- GlassBaseMap.lua
+++ GlassBaseMap.lua
@@ -8,46 +8,12 @@
 -- Wish me luck :)
 whitelist = {"ZeeDerpMaster","Sleetyy"}
 glass = peripheral.wrap("right")
-getfenv(("").gsub).playerList = {}
-nodes = {}
---
-for i=1,#getfenv(("").gsub).playerList do 
-table.remove(getfenv(("").gsub).playerList,1)
-end
---
-function table.contains(tab, ele)
-    for _, value in pairs(tab) do
-        if value == ele then
-            return true
+nodenum = 1
+repeat 
+    local x = getfenv(('node'..nodenum).gsub).prox
+    if x == true then
+        for i,v in pairs(masterPlayerTable) do 
+            print(v)
         end
     end
-    return false
-end
---
-
-
-
---
-getfenv(("").gsub).proxyVar = "id = os.getComputerID() sensor = peripheral.wrap('top') players = sensor.getPlayerNames() while true do if #players > 0 then for i,v in pairs(players) do table.insert(getfenv(('').gsub).playerList,players[i]..' '..id) print(getfenv(('').gsub).playerList[i]) end end sleep(1) end"
-sleep(1)
-while true do
-    for i,v in pairs(getfenv(("").gsub).playerList) do 
-        d = string.find(getfenv(("").gsub).playerList[i],1)
-        print(string.sub(getfenv(("").gsub).playerList[i],1,(d-1)))
-        if table.contains(whitelist,string.sub(getfenv(("").gsub).playerList[i],1,(d-1))) then
-            getfenv(("").gsub).playerList[i] = nil
-        end
-        realID = string.sub(getfenv(("").gsub).playerList[i],d,(d+4))
-        nodes[i] = realID
-        print(string.sub(getfenv(("").gsub).playerList[i],1,(d-1)).." is at "..realID)
-        if i > 10 then
-            i = 1
-        end
-    end
-    if #getfenv(("").gsub).playerList > 10 then
-        for i=1,#getfenv(("").gsub).playerList do 
-            table.remove(getfenv(("").gsub).playerList,1)
-        end
-    end
-    sleep(.1)
-end
+until x == nil
--- baseNode.lua
+++ baseNode.lua
@@ -1,4 +1,27 @@
 ---DEPRACATED---
 -- This is the node that will be distributed around the base.
-prog = getfenv(("").gsub).proxyVar
-loadstring(prog)()
+-- Variable per node
+-- if node == true, start writing, when false, clear. ezpz
+sensor = peripheral.wrap("top")
+closePlayers = sensor.getPlayerNames()
+masterPlayerTable = getfenv(("").gsub).playerList
+print("What node is this?")
+input = read()
+getfenv((input).gsub).prox = false
+while true do
+if #closePlayers > 0 then
+    getfenv((input).gsub).prox = true
+    for i=1,#closePlayers do
+        table.insert(masterPlayerTable,closePlayers[i])
+        if #closePlayers == 0 then
+            getfenv((input).gsub).prox = false
+            getfenv(("").gsub).playerList = {}
+            break
+        end
+    end
+end
+sleep(.1)
+end
+
+
+
