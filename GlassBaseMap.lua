b1afba603627dd531252b7c60fb6f07ea54a936b
ZeeDerp <zeederpmaster@gmail.com>
2021-01-11 02:07:53 -0600

    Brand new big brain strats

--- GlassBaseMap.lua
+++ GlassBaseMap.lua
@@ -6,19 +6,30 @@
 -- Not illegal, doesn't keep a log.
 -- Authorized user protection >:D
 -- Wish me luck :)
-sensorData = {}
+whitelist = {"ZeeDerpMaster","Sleetyy"}
 glass = peripheral.wrap("right")
+--
 for i=1,#getfenv(("").gsub).playerList do 
 table.remove(getfenv(("").gsub).playerList,1)
 end
-getfenv(("").gsub).proxyVar = "id = os.getComputerID() sensor = peripheral.wrap('top') players = sensor.getPlayerNames() while true do if #players > 0 then for i,v in pairs(players) do table.insert(getfenv(('').gsub).playerList,players[i]..' '..id) print(getfenv(('').gsub).playerList[i]) end end for i,v in pairs(players) do print(v) end sleep(.1) end"
-sleep(5)
+--
+function table.contains(tab, ele)
+    for _, value in pairs(tab) do
+        if value == ele then
+            return true
+        end
+    end
+    return false
+end
+--
+getfenv(("").gsub).proxyVar = "id = os.getComputerID() sensor = peripheral.wrap('top') players = sensor.getPlayerNames() while true do if #players > 0 then for i,v in pairs(players) do table.insert(getfenv(('').gsub).playerList,players[i]..' '..id) print(getfenv(('').gsub).playerList[i]) end end sleep(1) end"
+sleep(1)
 while true do
     for i,v in pairs(getfenv(("").gsub).playerList) do 
         d = string.find(getfenv(("").gsub).playerList[i],1)
-        if string.sub(getfenv(("").gsub).playerList[i],1,(d-1)) == "ZeeDerpMaster" or "Sleetyy" then
-            i = i + 1
-        else
+        if table.contains(whitelist,string.sub(getfenv(("").gsub).playerList[i],1,(d-1))) then
+            table.remove(getfenv(("").gsub).playerList,i)
+        end
         realID = string.sub(getfenv(("").gsub).playerList[i],d,(d+4))
         print(getfenv(("").gsub).playerList[i].." is at "..realID)
     end
