56a5754cf572b798ab35f7ae6ad62194e0de4926
ZeeDerp <zeederpmaster@gmail.com>
2021-01-11 00:37:38 -0600

    Brand new big brain strats

--- GlassBaseMap.lua
+++ GlassBaseMap.lua
@@ -2,7 +2,7 @@
 -- Small Scale display of top down area of base
 -- Sensor nodes every 16 blocks around the edge
 -- When someone enters the influence of a node, they are displayed on glasses
--- Not illegal, doesnt use sensor.sonicScan()
+-- Not illegal, doesnt use coordinate based tracking
 -- Not illegal, doesn't keep a log.
 -- Authorized user protection >:D
 -- Wish me luck :)
