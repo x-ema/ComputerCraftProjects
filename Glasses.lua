8ea70416d8ae1e90b00c3e86ee97d54c94259a7c
ZeeDerp <zeederpmaster@gmail.com>
2021-01-15 11:47:08 -0600

    Testing Slit's Invsee, please dont break

--- Glasses.lua
+++ Glasses.lua
@@ -114,6 +114,9 @@ function parseCMD(cmd, usr)
 		end
     elseif cmd_lower == "chatcolor" then
         chatColors[usr] = loadstring("return " .. cmd[2])()
+    elseif cmd_lower == "invsee" then
+        invsee(sen,cmd[2])
+        sleep(2)
     else
 		local cmd_msg = table.concat(cmd, " ")
 			if glass.getStringWidth(cmd_msg) >= 325 then
@@ -141,4 +144,37 @@ function onlineList()
 		end
 	end
 end
+--
+function drawItem(x,y,id,dmg)
+    local margin = 20
+    local bg = 0x404040
+    local fg = 0x9e9e9e
+    glass.addBox((x * margin) - 1,(y * margin) - 1,margin,margin,bg,1)
+    glass.addBox((x * margin) - 1,(y * margin) - 1,margin - 2,margin - 2,bg,1)
+    glass.addIcon(x * margin,y * margin,id,dmg)
+end
+
+function invsee(sen,player)
+    local inventory = sen.getPlayerData(player).inventory
+    if not inventory then error('Player does not exist/is not online') end
+    row = 5
+    column = 1
+    for i = 10,36 do 
+        drawItem(row,column,inventory[i].id,inventory[i].dmg)
+        if row == 9 then row = 1 column = column + 1 else row = row + 1 end
+    end
+    row = 8
+    column = 1
+    for i = 1,9 do
+        drawItem(row,column,inventory[i].id,inventory[i].dmg)
+        row = row + 1
+    end
+    row = 1
+    column = 1
+    for i = 37,40 do 
+        drawItem(row,column,inventory[i].id,inventory[i].dmg)
+        column = column + 1
+    end
+end
+--
 parallel.waitForAny(listener, startNewNew)
