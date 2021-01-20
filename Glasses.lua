d6253458bb6a874d6af5f41be5e73bfa95632b84
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-20 15:00:34 -0800

    Update Glasses.lua

--- Glasses.lua
+++ Glasses.lua
@@ -1,186 +1,111 @@
--- -- TODO:
--- 1.Clean-up 
--- 2.Turt Control
--- 3.Ender requester.
-glass = peripheral.wrap("right")
-sen = peripheral.wrap("top")
-maxLines = 7
-getfenv(("").gsub).glass_chat = {}
-messages = getfenv(("").gsub).glass_chat
-authedusers = {"ZeeDerpMaster", "Sleetyy", "icedfrappuccino", "korvuus", "SoundsOfMadness", "mpfthprblmtq","e"}
+local peripherals = {
+  mount = function (self,peripheral_name)
+    for _,p in pairs(peripheral.getNames()) do
+      if peripheral.getType(p) == peripheral_name then return peripheral.wrap(p) break else return false break end
+    end
+  end
+}
 
-chatColors = {}
-chatColors["ZeeDerpMaster"] = 0x3C93C2
-chatColors["Sleetyy"] = 0xFFFFFF
-chatColors["mpfthprblmtq"] = 0x800080
-chatColors["SoundsOfMadness"] = 0x883388
-
---
-for i = 1, maxLines do
-    table.insert(messages, "$$$$")
-end
---
-function split(str)
+local chat = {
+  sensor = peripherals:mount('openperipheral_sensor'),
+  glass = peripherals:mount('openperipheral_glassesbridge'),
+  if not glass or not sensor then error('missing peripheral...') end
+  max_lines = 7,
+  max_length = 325,
+  margin_top = 10,
+  margin_msg = 10,
+  getfenv(("").gsub).glass_chat = {},
+  messages = getfenv(("").gsub).glass_chat,
+  
+  authed = {
+    Sleetyy = {
+      name = 'Sleetyy',
+      color = 0xFFFFFF
+    },
+    ZeeDerpMaster = {
+      name = 'ZeeDerpMaster',
+      color = 0x3C93C2
+    },
+    icedfrappuccino = {
+      name = 'icedfrappuccino',
+      color = 0x883388
+    },
+    korvuus = {
+      name = 'korvuus',
+      color = 0xFFFFFF
+    },
+    SoundsOfMadness = {
+      name = 'SoundsOfMadness',
+      color = 0x883388
+    },
+    mpfthprblmtq = {
+      name = 'mpfthprblmtq',
+      color = 0x800080
+    },
+  },
+  
+  addMsg = function (self,msg) self.messages[#self.messages + 1] = msg end,
+  
+  removeMsg = function (self,num) table.remove(self.messages,num) end,
+  
+  cmds = {
+    nuke = function (self,msg,usr) getfenv(("").gsub).glass_chat = {} self.glass.clear() os.reboot() end,
+    maxlines = function (self,msg,usr)
+      for i = 1,max_lines do chat:removeMsg(1) end
+      self.max_lines = tonumber(msg[2])
+      for i = 1,max_lines do chat:addMsg('$$$$') end
+    end,
+    chatcolor = function (self,msg,usr) authed[usr].color = loadstring('return '..msg[2])() end
+  },
+  
+  split = function (self,str)
     words = {}
-    for word in str:gmatch("%S+") do
-        words[#words + 1] = word
-    end
-    return words
-end
---
-function table.contains(tab, ele)
-    for i = 1, #tab do
-        if tab[i] == ele then
-            return true
-        end
-    end
-    return false
-end
---
-function getName(message)
-    local name = nil
-    while true do
-        if string.find(message, ":") then
-            message = string.match(message, "(.*):")
-        else
-            name = message
-            break
-        end
-    end
-    return name
-end
---
-function nuke()
-    getfenv(("").gsub).glass_chat = {}
-    glass.clear()
-    shell.run("reboot")
-end
---
-function drawItem(x, y, id, dmg)
-    local margin = 20
-    local bg = 0x404040
-    local fg = 0x9e9e9e
-    glass.addBox((x * margin) - 1, (y * margin) - 1, margin, margin, bg, 1)
-    glass.addBox((x * margin) - 1, (y * margin) - 1, margin - 2, margin - 2, bg, 1)
-    glass.addIcon(x * margin, y * margin, id, dmg)
-end
---
-function invsee(sen, player)
-    local inventory = sen.getPlayerData(player).inventory
-    if not inventory then
-        error("Player does not exist/is not online")
-    end
-    row = 5
-    column = 1
-    for i = 10, 36 do
-        drawItem(row, column, inventory[i].id, inventory[i].dmg)
-        if row == 9 then
-            row = 1
-            column = column + 1
-        else
-            row = row + 1
-        end
-    end
-    row = 8
-    column = 1
-    for i = 1, 9 do
-        drawItem(row, column, inventory[i].id, inventory[i].dmg)
-        row = row + 1
-    end
-    row = 1
-    column = 1
-    for i = 37, 40 do
-        drawItem(row, column, inventory[i].id, inventory[i].dmg)
-        column = column + 1
-    end
-    sleep(2)
-end
---
-function authCheck()
-    currentUsers = glass.getUsers()
-    for i = 1, #currentUsers do
-        if table.contains(authedusers, currentUsers[i]) == false then
-            for i, v in pairs(currentUsers) do
-                print(v)
-            end
-            nuke()
-        else
-            print''
-        end
-    end
-end
-function listener()
-    while true do
-        authCheck()
-        local tEvent = {os.pullEventRaw()}
-        if tEvent[1] == "chat_command" then
-            cmd = split(tEvent[2])
-            user = tostring(tEvent[3])
-            parseCMD(cmd, user)
-        end
-    end
-end
---
-function startNewNew()
-    while true do
-        authCheck()
-        glass.clear()
-        height = (maxLines * 10)
-        glass.addBox(0, 20, 335, height, 0x000000, 0.5)
-        for i = 1, #messages do
-            pos = 10 + (i * 10)
-            message = messages[i]
-            color = chatColors[getName(message)]
-            glass.addText(5, pos, message, color)
-        end
-        onlineList()
-        sleep(0.1)
-    end
-end
---
-function parseCMD(cmd, usr)
-    local cmd_lower = cmd[1]:lower()
-    if cmd_lower == "maxlines" then
-        for i = 1, tonumber(maxLines) do
-            table.remove(messages, 1)
-        end
-        maxLines = tonumber(cmd[2])
-        for i = 1, tonumber(maxLines) do
-            table.insert(messages, "$$$$")
-        end
-    elseif cmd_lower == "chatcolor" then
-        chatColors[usr] = loadstring("return " .. cmd[2])()
-    elseif cmd_lower == "nuke" then
-        nuke()
-    elseif cmd_lower == "invsee" then
-        invsee(sen, cmd[2])
+    for word in str:gmatch('%S+') do words[#words + 1] = word end return words
+  end,
+  
+  contains = function (self,tab,val) for i = 1,#tab do if tab[i] == val then return true end return false end end,
+  
+  authCheck = function (self) for _,user in pairs(self.glass.getUsers()) do if not self.authed[user] then self.cmds.nuke() end end end,
+  
+  event_listen = function (self)
+    chat:authCheck()
+    _,msg,usr = os.pullEvent('chat_command')
+    msg = chat:split(msg)
+    if self.cmds[msg[1]:lower()] then
+      self.cmds[msg[1]:lower()](msg,usr)
     else
-        local cmd_msg = table.concat(cmd, " ")
-        if glass.getStringWidth(cmd_msg) > 325 then
-            cutMsgOne = string.sub(cmd_msg, 1, 48)
-            cutMsgTwo = string.sub(cmd_msg, 49, string.len(cmd_msg))
-            table.insert(messages, usr .. ": " .. cutMsgOne)
-            table.insert(messages, usr .. ": " .. cutMsgTwo)
-            table.remove(messages, 1)
-            table.remove(messages, 1)
-        else
-            table.insert(messages, usr .. ": " .. cmd_msg)
-            table.remove(messages, 1)
-        end
+      msg = usr..' : '..table.concat(msg,' ')
+      if self.glass.getStringWidth(table.concat(msg,' ')) > 325 then
+        chat:addMsg(table.concat(msg,' '):sub(1,48))
+        chat:addMsg(table.concat(msg,' '):sub(49,#msg))
+        chat:removeMsg(1)
+        chat:removeMsg(1)
+      else
+        chat:addMsg(table.concat(msg,' '))
+        chat:removeMsg(1)
+      end
     end
-end
---
-function onlineList()
+  end,
+  
+  main = function (self)
+    chat:authCheck()
+    self.glass.clear()
+    self.glass.addBox(0,20,335,self.max_lines * self.margin_top,0x000000,0.5)
+    for i = 1, #self.messages do
+      self.glass.addText(5, self.margin_top + (i * self.margin_msg),self.messages[i],self.messages[i]:sub(1,self.messages[i]:find(':') - 2).color)
+    end
     if #glass.getUsers() > 0 then
-        usrNum = #glass.getUsers()
-        usrNam = glass.getUsers()
-        glass.addBox(336, 20, 91, 60, 0x000000, 0.5)
-        for i = 1, usrNum do
-            h = 10 + (i * 10)
-            glass.addText(337, h, usrNam[i], chatColors[getName(usrNam[i])])
-        end
+      users = self.glass.getUsers()
+      self.glass.addBox(336,20,91,60,0xFFFFFF,0.5)
+      for i = 1, #users do
+        glass.addText(337,self.margin_top + (i,self.margin_msg),users[i],self.authed[users[i]].color)
+      end
     end
-end
---
-parallel.waitForAny(listener, startNewNew)
---
+    sleep(0.01)
+  end,
+  start = function (self) parallel.waitForAny(chat:main(),chat:event_listen()) end
+}
+  
+  
+  
+chat:start()
